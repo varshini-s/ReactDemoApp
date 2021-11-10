b50219c881a920ed7a633aa97d17025b34fb5b5f
varshini <dreamvarshini@gmail.com>
2021-11-10 23:55:33 +0530

    MODIFIED: using state property to display hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import './App.css';
 
-class App extends Component {
+class App extends Component 
+{
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+
+      title:'Hello from bridgelabz'
+       
+    }
+  }
+  
   render() {
     return (
       <div>
-        <h1>Hello from bridgelabz</h1>
+        <h1>{this.state.title}</h1>
       </div>
     )
   }
