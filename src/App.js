a972b7e3b71048cde65a11624e671f15b37cb3d7
varshini <dreamvarshini@gmail.com>
2021-11-11 00:05:46 +0530

    ADDED: bridgelabz logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-
+import logo from './assets/img/logo.png'
 class App extends Component 
 {
   constructor(props) {
@@ -17,8 +17,10 @@ class App extends Component
     return (
       <div>
         <h1>{this.state.title}</h1>
+        <img src={logo}
+          alt="The Bridgelabz logo: a Bridge to Employee through lab works"/>
       </div>
-    )
+    );
   }
 }
 
