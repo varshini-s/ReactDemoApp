10a14ffcde9883149115cea01c646040bea6ec50
varshini <dreamvarshini@gmail.com>
2021-11-11 00:10:30 +0530

    MODIFIED: onclick event to redirect to bridgelabz page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import logo from './assets/img/logo.png'
 class App extends Component 
 {
+  url='https://www.bridgelabz.com/'
   constructor(props) {
     super(props)
   
@@ -12,12 +13,17 @@ class App extends Component
        
     }
   }
+
+  onClick=($event)=>{
+    console.log("save button is clicked!",$event);
+    window.open(this.url,"_blank");
+  }
   
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo}
+        <img src={logo} onClick={this.onClick}
           alt="The Bridgelabz logo: a Bridge to Employee through lab works"/>
       </div>
     );
