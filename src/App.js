74a7421d918abcb813811b883add6aaf11abc153
varshini <dreamvarshini@gmail.com>
2021-11-11 01:10:26 +0530

    ADDED: content of the page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component
   
     this.state = {
 
-      userName:''
+      userName:'',
+      nameError:''
        
     }
   }
@@ -21,21 +22,56 @@ class App extends Component
 
   onNameCHange=(event)=>{
     console.log("value is",event.target.value);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+
     this.setState({userName:event.target.value})
+    if(nameRegex.test(event.target.value))
+    {
+      this.setState({nameError:''})
+    }
+    else
+    {
+      this.setState({nameError:'Name is incorrect'})
+
+    }
   }
   
   render() {
     return (
-      <>
-      <div>
-        <h1>Hello {this.state.userName} from Bridgelabz</h1>
-        <img src={logo} onClick={this.onClick}
-          alt="The Bridgelabz logo: a Bridge to Employee through lab works"/>
+      <div class="form">
+        <div>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
+          
+        </div>
+        <div class="img-content">
+          <img src={logo} onClick={this.onClick}
+            alt="The Bridgelabz logo: a Bridge to Employee through lab works"/>
+          <input onChange={this.onNameCHange}/>
+          <span className="error-output">{this.state.nameError}</span>
+          <div class="content">
+
+          <p>At Bridgelabz,we're a techie community of</p>
+                  <ul>
+                      <li>technologists</li>
+                      <li>thinkers</li>
+                      <li>builders</li>
+                  </ul>
+                  <p>
+                      Working togeter to keep the tech Employability of engineers alive and
+                      accesible,so Tech Companies worldwide can get contributors and
+                      creators for Technology solutions.We believe this act of human
+                      collaboration across an Employability platform is essential to individual
+                      growth and our collective future.
+                  </p>
+                  <p>
+                      To know about us,visit <a href="https://www.bridgelabz.com/">Bridgelabz</a> to learn even more about
+                      our
+                      mission i.e <strong> Employability to all</strong>
+                  </p>
+          </div >
+          
+        </div>
       </div>
-      <div>
-        <input onChange={this.onNameCHange}/>
-      </div>
-      </>
     );
   }
 }
